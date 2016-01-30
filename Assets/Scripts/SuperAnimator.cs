f071db596ef5b7a8e6bfde54d417df35079b9d97
AbrahamXD53 <mp2014000@gmail.com>
2016-01-30 00:58:50 -0600

    Carpetas

---
+++ Assets/Scripts/SuperAnimator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SuperAnimator : MonoBehaviour {
+
+    public Vector3 rotation;
+    public Vector3 scale;
+    public float speedScale=1;
+    public float speedRotation=1;
+
+    private Vector3 initialScale;
+    private Vector3 initialRotation;
+	void Start () {
+        initialRotation = transform.rotation.eulerAngles;
+        initialScale = transform.localScale;
+	}
+	
+	void Update () {
+        transform.localScale = initialScale+ (scale * Mathf.Sin(speedScale * Time.timeSinceLevelLoad));
+        transform.localRotation= Quaternion.Euler(initialRotation+ (rotation * Mathf.Sin(speedRotation * Time.timeSinceLevelLoad)));
+    }
+}
