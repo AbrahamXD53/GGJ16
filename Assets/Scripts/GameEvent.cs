d587ddeddbf8cfc0ac92c565e5c77f5f1c6530a9
AbrahamXD53 <mp2014000@gmail.com>
2016-01-30 13:03:29 -0600

    funciona por favor

--- Assets/Scripts/GameEvent.cs
+++ Assets/Scripts/GameEvent.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 
-public class GameEvent : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public class GameEvent {
+     
+    Dictionary<Reaction, int> reactions;
+    Action<GameManager> actionTrue;
+    Action<GameManager> actionFalse;
+    public GameEvent(Dictionary<Reaction, int> reactions, Action<GameManager> actionSuccess, Action<GameManager> actionFail)
+    {
+        this.actionTrue = actionSuccess;
+        this.actionFalse = actionFail;
+		this.reactions = reactions;
+    }
+    public void Apply(GameManager man, Reaction react)
+    {
+		int randomNumber = UnityEngine.Random.Range (0, 101);
+		if (react != null && reactions.ContainsKey (react) && randomNumber <= reactions [react]) {
+			actionTrue (man);
+			return;
+		}
+        actionFalse(man);
+    }
 }
