816947460b858ac80c271236c4a7e96537d8696f
AbrahamXD53 <mp2014000@gmail.com>
2016-01-30 13:57:45 -0600

    Test
    NI idea

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public AudioClip[] effect;
+    public AudioClip[] music;
+    private AudioSource audioSource;
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnLevelWasLoaded(int level)
+    {
+        print("level loaded: "+level);
+        audioSource = GetComponent<AudioSource>();
+
+        if (audioSource != null)
+        {
+            if (level == 0)
+            {
+                audioSource.clip = music[0];
+                audioSource.Play();
+            }
+        }
+    }
+
+}
--- Assets/Scripts/Warning.cs
+++ Assets/Scripts/Warning.cs
@@ -1,26 +1,36 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class Warning : MonoBehaviour {
-
+public class Warning : MonoBehaviour
+{
 
     private Camera m_camera;
-    public float epCount=10;
+    public float epCount = 10;
     public float duration = 3;
     private float interval;
     public Text txtWarning;
-	void Start() {
+    private bool canClic = false;
+
+    IEnumerator coldDown()
+    {
+        canClic = false;
+        yield return new WaitForSeconds(2f);
+        canClic = true;
+    }
+    void Start()
+    {
         m_camera = GetComponent<Camera>();
         interval = duration / epCount;
+        StartCoroutine(coldDown());
     }
-	
-	// Update is called once per frame
-	void Update () {
-	    if(Input.anyKeyDown || (Input.touchCount>0 && Input.GetTouch(0).phase==TouchPhase.Began))
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) && canClic)
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
             if (epCount > 1)
             {
                 iTween.ShakePosition(txtWarning.transform.parent.GetChild(1).gameObject, new Vector3(Random.Range(0, 20), Random.Range(0, 20), 0), duration * 10);
@@ -28,33 +38,24 @@ public class Warning : MonoBehaviour {
             }
             else
             {
-                //Cargar la escena chida
-                print("Cargar la chida");
                 SceneManager.LoadScene(1);
-                SceneManager.LoadScene(2,LoadSceneMode.Additive);
+                SceneManager.LoadScene(2, LoadSceneMode.Additive);
             }
-=======
-            iTween.ShakePosition(txtWarning.transform.parent.GetChild(1).gameObject, new Vector3(Random.Range(0, 20), Random.Range(0, 20), 0), duration*10);
-            StartCoroutine(Epilepsia());
->>>>>>> origin/master
-=======
-            iTween.ShakePosition(txtWarning.transform.parent.GetChild(1).gameObject, new Vector3(Random.Range(0, 20), Random.Range(0, 20), 0), duration*10);
-            StartCoroutine(Epilepsia());
->>>>>>> a624c4821d7c5372ae7396ba40a56b32841eb412
         }
-	}
-    
+
+    }
+
     IEnumerator Epilepsia()
     {
-        //print("called");
-        while((epCount--)>1)
+        while ((epCount--) > 1)
         {
-            m_camera.backgroundColor = new Color(Random.Range(20,100)/100f, Random.Range(20, 100) / 100f, Random.Range(20, 100) / 100f, 1);
-            //print(m_camera.backgroundColor);
+            m_camera.backgroundColor = new Color(Random.Range(20, 100) / 100f, Random.Range(20, 100) / 100f, Random.Range(20, 100) / 100f, 1);
+            print(m_camera.backgroundColor);
             yield return new WaitForSeconds(interval);
         }
-        m_camera.backgroundColor = new Color(1,1,1);
+        m_camera.backgroundColor = new Color(1, 1, 1);
         txtWarning.text = "Ah!... También puede causar epilepsia a personas fotosensibles no me pregunten no soy 100tifiko";
+        StartCoroutine(coldDown());
         yield return 0;
     }
 }
