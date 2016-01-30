c81c0a93effd698ac9b9b84fa198b754f6209bf6
brody7u7 <brodyn.n@hotmail.com>
2016-01-30 01:46:49 -0600

    Button, Pee Bar

---
+++ Assets/TestButton/AbilitieButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbilitieButton : MonoBehaviour {
+
+    private RectTransform fade;
+    
+	// Use this for initialization
+	void Start () {
+        fade = GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
